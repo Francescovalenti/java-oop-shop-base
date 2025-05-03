3dd54ec65bd60e6ee41402a0c499f15ab48bf1dd
francescovalenti <francescovalenti83@outlook.it>
2025-05-03 11:00:20 +0200

    feat: inserimento nome,descrizione e formula codice prodotto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Prodotto Maglietta  = new Prodotto() ;
     }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,9 +1,20 @@
+import java.util.Random;
+
 public class Prodotto {
     public String Nome;
     public String Descrizione;
     public int Codice;
     public int prezzo;
     public int iva;
+
+    public Prodotto (String Nome,String Descrizione, int Codice, int prezzo,int iva){
+        this.Nome="Adidas Originals 3striscie nere";
+        this.Descrizione="Brand: Adidas Originals Righe Logo Composizione: cotone Collo: girocollo";
+        Random rand = new Random();
+        this.Codice = rand.nextInt(90000) + 10000;
+        this.prezzo=
+        this.iva=
+    }
 
     }
 
