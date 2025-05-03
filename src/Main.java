1999a4661cb657cf9d0dd1be9bf2d578dff0ae5d
francescovalenti <francescovalenti83@outlook.it>
2025-05-03 11:53:53 +0200

    feat: fine maglietta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Prodotto Maglietta  = new Prodotto() ;
+        Prodotto Maglietta  = new Prodotto("Adidas Originals 3striscie nere","Composizione: cotone Collo: girocollo", 0,20,22 );
+        System.out.println(Maglietta.venduta());
+        System.out.println("Il prezzo di base: " + Maglietta.getPrezzo());
+        System.out.println("il prezzo con iva: "  + Maglietta.getPrezzoIva());
     }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -4,17 +4,26 @@ public class Prodotto {
     public String Nome;
     public String Descrizione;
     public int Codice;
-    public int prezzo;
+    public int Prezzo;
     public int iva;
 
     public Prodotto (String Nome,String Descrizione, int Codice, int prezzo,int iva){
-        this.Nome="Adidas Originals 3striscie nere";
-        this.Descrizione="Brand: Adidas Originals Righe Logo Composizione: cotone Collo: girocollo";
+        this.Nome=Nome;
+        this.Descrizione=Descrizione;
         Random rand = new Random();
         this.Codice = rand.nextInt(90000) + 10000;
-        this.prezzo=
-        this.iva=
+        this.Prezzo=prezzo;
+        this.iva=iva;
     }
+     public int getPrezzo (){
+        return Prezzo;
+     }
+     public int getPrezzoIva(){
+        return Prezzo + ((Prezzo * iva ) /100);
+     }
 
+     public String venduta(){
+        return Codice + "-" + Nome + " " + Descrizione;
+     }
     }
 
