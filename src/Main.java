ddb42227bac5b0e70b27b5eac7fc552cc1534dc6
francescovalenti <francescovalenti83@outlook.it>
2025-05-03 12:01:41 +0200

    feat: fine

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Prodotto Maglietta  = new Prodotto("Adidas Originals 3striscie nere","Composizione: cotone Collo: girocollo", 0,20,22 );
         System.out.println(Maglietta.venduta());
-        System.out.println("Descrizione prodotto:" + Maglietta.Descrizione);
+        System.out.println("Descrizione prodotto:"+" " + Maglietta.Descrizione);
         System.out.println("Il prezzo di base: " + Maglietta.getPrezzo());
         System.out.println("il prezzo con iva: "  + Maglietta.getPrezzoIva());
     }
