93c9ceb22aef5b9ac879f977f57e485d25ed9ee2
francescovalenti <francescovalenti83@outlook.it>
2025-05-03 13:36:04 +0200

    feat: sistemato con prezzo decimale

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Prodotto Maglietta  = new Prodotto("Adidas Originals 3striscie nere","Composizione: cotone Collo: girocollo", 0,20,22 );
+        Prodotto Maglietta  = new Prodotto("Adidas Originals 3striscie nere","Composizione: cotone Collo: girocollo", 0,20.0,22 );
         System.out.println(Maglietta.venduta());
         System.out.println("Descrizione prodotto:"+" " + Maglietta.Descrizione);
         System.out.println("Il prezzo di base: " + Maglietta.getPrezzo());
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,29 +1,31 @@
 import java.util.Random;
 
 public class Prodotto {
-    public String Nome;
-    public String Descrizione;
-    public int Codice;
-    public int Prezzo;
-    public int iva;
+   public String Nome;
+   public String Descrizione;
+   public int Codice;
+   public double Prezzo;
+   public double iva;
 
-    public Prodotto (String Nome,String Descrizione, int Codice, int prezzo,int iva){
-        this.Nome=Nome;
-        this.Descrizione=Descrizione;
-        Random rand = new Random();
-        this.Codice = rand.nextInt(90000) + 10000;
-        this.Prezzo= prezzo;
-        this.iva=iva;
-    }
-     public int getPrezzo (){
-        return Prezzo;
-     }
-     public int getPrezzoIva(){
-        return Prezzo + ((Prezzo * iva ) /100);
-     }
+   public Prodotto(String Nome, String Descrizione, int Codice, double prezzo, double iva) {
+      this.Nome = Nome;
+      this.Descrizione = Descrizione;
+      Random rand = new Random();
+      this.Codice = rand.nextInt(90000) + 10000;
+      this.Prezzo = prezzo;
+      this.iva = iva;
+   }
 
-     public String venduta(){
-        return Codice + "-" + Nome;
-     }
-    }
+  
+   public double getPrezzo() {
+      return Prezzo;
+   }
 
+   public double getPrezzoIva() {
+      return Prezzo + ((Prezzo * iva) / 100);
+   }
+
+   public String venduta() {
+      return Codice + "-" + Nome;
+   }
+}
